41792ba6ba57609e47bba9c99e3115ce51bbbe11
Débora <debora.r.serra@gmail.com>
2024-06-19 09:09:52 -0300

    feat: add conta poupanca

--- src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,27 @@
 package com.trybe.acc.contapoupanca;
 
 public class ContaPoupanca {
+  private  double saldo;
+  private String titularConta;
+
+  public ContaPoupanca(String titularConta, double saldo) {
+    this.saldo = saldo;
+    this.titularConta = titularConta;
+  }
+
+  public void depositar(double value) {
+  saldo += value;
+}
+
+public void sacar(double value) {
+  saldo -= value;
+}
+
+public double mostrarSaldo() {
+  return saldo;
+}
+  public  String mostrarTitularConta() {
+  return titularConta;
+  }
 
 }
